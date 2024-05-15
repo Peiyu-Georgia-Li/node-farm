f22a2c0fb8ef118299873b84b46ee03271b60f35
Peiyu-Georgia-Li <lipy4688@gmail.com>
2024-05-15 23:02:48 +0800

    build and use our own module & install slugify and nodeman

--- index.js
+++ index.js
@@ -35,20 +35,7 @@ const fs = require('fs');
 const url = require('url');
 const http =  require('http');
 
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%PRICE%}/g, product.price)
-    output = output.replace(/{%DISCRIPTIONS%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-
-    return output
-}
+const replaceTemplate = require('./modules/replaceTemplate')
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/temp-overview.html`, 'utf8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/temp-product.html`, 'utf8');
